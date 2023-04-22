59e02edce4137a6fd380e8a6d9258b76cf8cce2c
MiKe-721 <mikecryptoacc267@gmail.com>
2023-04-22 23:18:39 +0900

    frontend:変更機能実装

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -49,12 +49,23 @@ const connectWallet = async () => {
     });
     console.log(`account ${accounts[0]}`)
     setAccount(accounts[0])
+
+    ethereum.on('accountsChanged', checkAccountChanged);
+    ethereum.on('chainChanged', checkChainId);
   } catch (err) {
     console.log(err)
   }
 }
 
-
+const checkAccountChanged = () => {
+  setAccount('');
+  setNftOwner(false);
+  setItems([]);
+  setTokenBalance('');
+  setBankBalance('');
+  setBankTotalDeposit('');
+  setInputData({ transferAddress: '', transferAmount: '', depositAmount: '', withdrawAmount: '' });
+}
 
   useEffect(() => {
     checkMetaMaskInstalled();
